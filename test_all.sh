a879750fd334f8a312ae61ff44ef7d80d930d161
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-10-01 17:28:32 +0200

    Add main script to run all tests

---
+++ test_all.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+TESTS=$(ls tests/)
+
+#echo $TESTS
+
+TOTAL=0
+PASSED=0
+
+function line() {
+printf "=%.0s" {0..80}
+echo
+}
+
+line
+printf "Starting test suite\n"
+line
+
+for TEST in $TESTS; do
+  TOTAL=$((TOTAL + 1))
+
+  ./test.sh "tests/$TEST" 2>&1 > /dev/null
+
+  if [[ $? -eq 0 ]]; then
+    printf "%-50s %30s\n" "$TEST" "OK"
+    PASSED=$((PASSED + 1))
+  else
+    printf "%-50s %30s\n" "$TEST" "FAIL"
+  fi
+done
+
+line
+printf "%-50s %30s\n" "TOTAL" "$PASSED/$TOTAL"
+
+if [[ "$PASSED" == "$TOTAL" ]]; then
+  printf "%-50s %30s\n" "RESULT" "SUCCESS"
+  line
+  exit 0
+else
+  printf "%-50s %30s\n" "RESULT" "FAILURE"
+  line
+  exit 1
+fi
