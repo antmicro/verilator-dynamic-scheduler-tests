be55b815475e1e258acef4ad28213d3a6ee904ac
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-12-02 08:48:54 +0100

    Revert "Increase the timeout even further."
    This reverts commit cb851f8471b0ed4afcf1d27c9fe95c6f3ee03d64.
    
    GHA is not capable of running the tests with such a large timeout value.

--- gen_robot.py
+++ gen_robot.py
@@ -14,7 +14,7 @@ with open("templates/buitin.robot", "r") as t:
     builtin = Template(t.read())
 
 test_cases = []
-timeout = "900s"
+timeout = "300s"
 
 for t in sorted(glob("tests/*")):
     test_cases.append(dedicated.render(test=t,
