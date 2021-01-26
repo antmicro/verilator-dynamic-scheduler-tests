31effa6b3579d62ccdd07bc02f1485719d68709d
Krzysztof Bieganski <kbieganski@antmicro.com>
2021-01-26 17:21:33 +0100

    Extend the dependent NBA test

--- tests/dependent_nba/test.sv
+++ tests/dependent_nba/test.sv
@@ -3,6 +3,40 @@ module t(clk);
 
    integer cyc = 0;
 
+   reg [7:0] a;
+   reg [127:0] b;
+
+   always @ (posedge clk) begin
+      cyc <= cyc + 1;
+      if (cyc == 0) begin
+         a <= 8'hFF;
+         a[7] <= 1'b0;
+      end
+      else if (cyc == 1) begin
+         $write("a = %x\n", a);
+         if (a != 8'h7F) $stop;
+      end
+      else if (cyc == 2) begin
+         b <= 128'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+         b[127] <= 1'b0;
+      end
+      else if (cyc == 3) begin
+         $write("b = %x\n", b);
+         if (b != 128'h7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) $stop;
+      end
+      else if (cyc > 3) begin
+         $write("*-* All Finished *-*\n");
+         $finish;
+      end
+   end
+
+endmodule
+
+/*module t(clk);
+   input clk;
+
+   integer cyc = 0;
+
    reg [127:0] a;
 
    always @ (posedge clk) begin
@@ -22,3 +56,4 @@ module t(clk);
    end
 
 endmodule
+*/
