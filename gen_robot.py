c7962604e0f8921cd8a6ab2972e293202a630519
Tomasz Gorochowik <tgorochowik@antmicro.com>
2021-01-29 14:54:12 +0100

    Add "perf" tests group

--- gen_robot.py
+++ gen_robot.py
@@ -41,14 +41,25 @@ for t in sorted(glob("tests/*")):
 with open("robot_tests/dedicated.robot", "w") as t:
     t.write(suite.render(test_cases="\n".join(test_cases)))
 
+# tests related to optimization that could be non-functional now
+tests_opt = [
+    "t/t_case_huge.pl", "t/t_case_huge_prof.pl", "t/t_dedupe_clk_gate.pl",
+    "t/t_merge_cond.pl", "t/t_var_life.pl", "t/t_mem_shift.pl"
+]
+
+# tests related to performance, sometimes fail in the CI but not on a regular
+# machine
+tests_perf = ["t/t_a3_selftest.pl"]
 
 test_cases = []
 for t in sorted(glob("verilator/test_regress/t/t_*pl")):
     t = t[len("verilator/test_regress/"):]
-    if t in {"t/t_case_huge.pl", "t/t_case_huge_prof.pl", "t/t_dedupe_clk_gate.pl", "t/t_merge_cond.pl", "t/t_var_life.pl", "t/t_mem_shift.pl"}:
+    if t in tests_opt:
         tags = "opt"
     elif "t/t_dist_" in t:
         tags = "dist"
+    elif t in tests_perf:
+        tags = "perf"
     elif "bad" in t:
         tags = "should_fail"
     else:
