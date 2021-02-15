2780aeb0e8e2856f7e795295c30f9550ffedbbd6
Tomasz Gorochowik <tgorochowik@antmicro.com>
2021-02-15 15:56:09 +0100

    Add new tests to perf and opt groups

--- gen_robot.py
+++ gen_robot.py
@@ -42,12 +42,27 @@ with open("robot_tests/dedicated.robot", "w") as t:
 # tests related to optimization that could be non-functional now
 tests_opt = [
     "t/t_case_huge.pl", "t/t_case_huge_prof.pl", "t/t_dedupe_clk_gate.pl",
-    "t/t_merge_cond.pl", "t/t_var_life.pl", "t/t_mem_shift.pl"
+    "t/t_merge_cond.pl", "t/t_var_life.pl", "t/t_mem_shift.pl",
+    "t/t_inst_tree_inl0_pub1.pl", "t/t_inst_tree_inl0_pub1_norelcfuncs.pl",
+    "t_unopt_combo_isolate_vlt.pl", "t_alw_splitord.pl", "t_cellarray.pl",
+    "t_reloop_cam.pl", "t_alw_split.pl", "t_alw_reorder.pl", "t_merge_cond.pl",
+    "t_dedupe_seq_logic.pl", "t_alw_split_rst.pl", "t_clk_gater.pl",
+    "t_alw_nosplit.pl", "t_gate_chained.pl", "t_alw_noreorder.pl",
+    "t_unopt_combo_isolate.pl"
 ]
 
 # tests related to performance, sometimes fail in the CI but not on a regular
 # machine
-tests_perf = ["t/t_a3_selftest.pl", "t/t_debug_graph_test.pl"]
+tests_perf = [
+    "t/t_a3_selftest.pl", "t/t_debug_graph_test.pl", "t/t_case_huge.pl",
+    "t/t_case_huge_prof.pl", "t/t_unopt_array_csplit.pl", "t/t_split_var_0.pl",
+    "t/t_split_var_2_trace.pl", "t/t_math_signed.pl",
+    "t/t_interface_gen7_noinl.pl", "t_inst_tree_inl0_pub0.pl",
+    "t_inst_tree_inl0_pub1.pl", "t/t_inst_tree_inl0_pub1_norelcfuncs.pl",
+    "t/t_inst_tree_inl1_pub1.pl", "t/t_final.pl", "t/t_gen_genblk_noinl.pl",
+    "t/t_gate_chained.pl"
+]
+
 
 test_cases = []
 for t in sorted(glob("verilator/test_regress/t/t_*pl")):
@@ -57,8 +72,10 @@ for t in sorted(glob("verilator/test_regress/t/t_*pl")):
 
     if t in tests_opt:
         tags.append("opt")
+
     if "t/t_dist_" in t:
         tags.append("dist")
+
     if t in tests_perf or "bench" in t:
         tags.append("perf")
 
@@ -67,7 +84,8 @@ for t in sorted(glob("verilator/test_regress/t/t_*pl")):
     else:
         tags.append("should_pass")
 
-    test_cases.append(builtin.render(test=t, tags="    ".join(tags), timeout=timeout))
+    test_cases.append(
+        builtin.render(test=t, tags="    ".join(tags), timeout=timeout))
 
 with open("robot_tests/builtin.robot", "w") as t:
     t.write(suite.render(test_cases="\n".join(test_cases)))
