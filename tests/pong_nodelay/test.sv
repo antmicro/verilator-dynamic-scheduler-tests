8ae3f6c13383519e874b3c444ded8ac8e93b3d7f
Krzysztof Bieganski <kbieganski@antmicro.com>
2021-12-07 15:40:12 +0100

    Add pong test version without delays
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

---
+++ tests/pong_nodelay/test.sv
@@ -0,0 +1,25 @@
+/* verilator lint_off INFINITELOOP */
+module t;
+   event ping;
+   event pong;
+
+   int cnt = 0;
+
+   initial
+       forever begin
+           @ping $write("ping\n");
+           ->pong;
+       end
+
+   initial
+       forever begin
+           @pong $write("pong\n");
+           cnt++;
+           if (cnt >= 10) begin
+               $write("*-* All Finished *-*\n");
+               $finish;
+           end else ->ping;
+       end
+
+   initial ->ping;
+endmodule
